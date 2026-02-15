fc0aad467177fb336252e6461151b4579ef64032
supawitpui-hub <supawit.pui@spumail.net>
2026-02-15 13:11:13 +0700

    Add files via upload

---
+++ LAB4/L4app.js
@@ -0,0 +1,27 @@
+console.log("JS ทำงานแล้ว");
+
+const smallimg = document.querySelectorAll(".gallery img");
+const fullimg = document.querySelector(".full-image");
+const modal = document.querySelector(".modal");
+
+smallimg.forEach(img => {
+    img.addEventListener("click", () => {
+        const fullsize = img.getAttribute("alt");
+        const path = `foods-images/full/${fullsize}.jpg`;
+
+        fullimg.src = path;
+        modal.classList.add("open");
+    });
+});
+
+modal.addEventListener("click", (e) => {
+    if (e.target.classList.contains("modal")) {
+        modal.classList.remove("open");
+    }
+});
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+        modal.classList.remove("open");
+    }
+});
